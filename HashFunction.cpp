08a16f30942b5dbc2d77acb04639dc2c2ae32b09
Eisbar <eisbarbatutus@gmail.com>
2021-07-26 06:25:16 +0700

    first version

---
+++ HashFunction.cpp
@@ -0,0 +1,27 @@
+#include "Header.h"
+
+vector<Company> ReadCompany(string file_name)
+{
+    fstream input(file_name.c_str());
+    vector<Company> res;
+    string company;
+    while (!input.eof())
+    {   
+      
+        getline(input, company);
+        stringstream ss(company);
+        Company temp;
+        getline(ss, temp.name, '|');
+        getline(ss,temp.profit_tax, '|');
+        getline(ss, temp.address, '|');
+        res.push_back(temp);
+        
+    }
+    input.close();
+    return res;
+}
+
+long long HashString(string company_name)
+{
+    
+}
