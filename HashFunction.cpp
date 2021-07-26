73ef84decb14ec6248acb081b59dfb8893e88768
Eisbar <eisbarbatutus@gmail.com>
2021-07-26 10:43:40 +0700

    Syntax fix

--- HashFunction.cpp
+++ HashFunction.cpp
@@ -6,22 +6,23 @@ vector<Company> ReadCompany(string file_name)
     vector<Company> res;
     string company;
     while (!input.eof())
-    {   
-      
+    {
+
         getline(input, company);
         stringstream ss(company);
         Company temp;
         getline(ss, temp.name, '|');
-        getline(ss,temp.profit_tax, '|');
+        getline(ss, temp.profit_tax, '|');
         getline(ss, temp.address, '|');
         res.push_back(temp);
-        
     }
     input.close();
     return res;
 }
 
 long long HashString(string company_name)
-{
-    //Xin lỗi mn function này hơi fail :((
+{   string temp;
+    int p = 31, m = pow(10,9) + 9;
+    //if(company_name.size() < 20)()
+    
 }
