1f1fb42707d0419ad84854366af3aecba1dfe29e
Eisbar <eisbarbatutus@gmail.com>
2021-07-29 15:06:42 +0700

    Implement binary tree travelsal

--- Binary Tree/BinaryTree.cpp
+++ Binary Tree/BinaryTree.cpp
@@ -1,4 +1,43 @@
 #include "Header.h"
 
-//Duyệt NLR
-void NLR (Node * pRoot)
+//Duyệt NLR (Node-Left-Right):
+/*In ra node gốc
+Duyệt cây bên trái
+Duyệt cây bên phải
+*/
+void NodeLeftRight(Node *pRoot)
+{
+    if (pRoot == NULL)
+        return;
+    cout << pRoot->key;
+    NodeLeftRight(pRoot->left);
+    NodeLeftRight(pRoot->right);
+}
+
+//Duyệt LNR (Left-Node-Right):
+/*Duyệt cây bên trái trước
+In ra node gốc 
+Duyệt cây bên phải 
+*/
+void LeftNodeRight(Node *pRoot)
+{
+    if (pRoot == NULL)
+        return;
+    LeftNodeRight(pRoot->left);
+    cout << pRoot->key;
+    LeftNodeRight(pRoot->right);
+}
+
+//Duyệt LRN (Left-Right-Node):
+/* Duyệt cây bên trái
+Duyệt cây bên phải 
+In ra node gốc 
+*/
+void LeftRightNode(Node *pRoot)
+{
+    if (pRoot == NULL)
+        return;
+    LeftRightNode(pRoot->left);
+    LeftRightNode(pRoot->right);
+    cout << pRoot->key;
+}
--- Binary Tree/Header.h
+++ Binary Tree/Header.h
@@ -8,9 +8,14 @@
 using namespace std;
 
 struct Node{
-    string key;
-    string 
-}
+    int key;
+    Node *left, *right; 
+};
+
+//Binary Traversal Functions: Các hàm duyệt cây nhị phân
+void NodeLeftRight(Node *pRoot);
+void LeftNodeRight(Node *pRoot);
+void LeftRightNode(Node *pRoot);
 
 
 #endif
