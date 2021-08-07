a1542674541aa9c94370a98dedf412280bd7e6e4
Eisbar <eisbarbatutus@gmail.com>
2021-08-07 08:35:57 +0700

    completed AVL Insert Node

--- AVL Tree/AVLTree_Function.cpp
+++ AVL Tree/AVLTree_Function.cpp
@@ -48,6 +48,27 @@ void Insert(Node *&pRoot, int value)
     if (pRoot->key < value)
         Insert(pRoot->right, value);
     int balance = Height(pRoot->left)  - Height(pRoot->right);
-
+    if(balance > 1){ //Mất cân bằng trái
+        //Trường hợp mất cân bằng trái-trái
+        if(Height(pRoot->left->left) > Height(pRoot->left->right)){
+            rightRotate(pRoot);
+        }
+        //Trường hợp mất cân bằng trái-phải
+        if(Height(pRoot->left->left) < Height(pRoot->left->right)){
+            leftRotate(pRoot->left);
+            rightRotate(pRoot);
+        }
+    }
+    if(balance < -1){//Mất cân bằng phải
+        //Trường hợp mất cân bằng phải phải
+        if(Height(pRoot->right->right) > Height(pRoot->right->left)){
+            leftRotate(pRoot);
+        }
+        //Trường hợp mất cân bằng trái trái
+        if(Height(pRoot->right->right) < Height(pRoot->right->left)){
+            rightRotate(pRoot->right);
+            leftRotate(pRoot); 
+        }
+    }
 
 }
